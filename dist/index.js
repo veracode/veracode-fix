37412dad125769c04404896b6aa7516031f55526
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2024-05-25 15:03:16 +0200

    adding batch annotations

--- dist/index.js
+++ dist/index.js
@@ -46716,69 +46716,67 @@ function updateCheckRunUpdateBatch(options, batchFixResults, flawInfo) {
             console.log('End line: ' + end_line);
             console.log('Batch Fix Results:');
             console.log(batchFixResults);
-            /*
-            
-                    //Let's check if there are multiple hunks on the first fix result
-                    let hunks = 0
-                    if (batchFixResults[0].indexOf('@@') > 0){
-                        //first remove the first part of the result that include the file names and path, we don't need that for the annotation
-                        const firstFixResult = batchFixResults[0]
-                        const cleanedResults = firstFixResult.replace(/^---.*$\n?|^\+\+\+.*$\n?/gm, '');
-                        const hunks = cleanedResults.split(/(?=@@ -\d+,\d+ \+\d+,\d+ @@\n)/);
-                        console.log('hunks:')
-                        console.log(hunks)
-                        const hunksCount = hunks.length
-                        console.log('Number of hunks: '+hunksCount)
-            
-                       
-                        for (let i = 0; i < hunksCount; i++) {
-                            
-                            const hunkLines = hunks[i].split('\n');
+            //Let's check if there are multiple hunks on the first fix result
+            let hunks = 0;
+            for (let key in batchFixResults.results) {
+                let patches = batchFixResults.results[key].patch;
+                for (let i = 0; i < patches.length; i++) {
+                    let patch = patches[i];
+                    console.log('Patch:');
+                    console.log(patch);
+                    if (patch.indexOf('@@') > 0) {
+                        const cleanedPatch = patch.replace(/^---.*$\n?|^\+\+\+.*$\n?/gm, '');
+                        const hunks = cleanedPatch.split(/(?=@@ -\d+,\d+ \+\d+,\d+ @@\n)/);
+                        console.log('hunks:');
+                        console.log(hunks);
+                        const hunksCount = hunks.length;
+                        console.log('Number of hunks: ' + hunksCount);
+                        for (let j = 0; j < hunksCount; j++) {
+                            const hunkLines = hunks[j].split('\n');
                             const hunkHeader = hunkLines[0];
                             const hunkHeaderMatch = hunkHeader.match(/@@ -(\d+),\d+ \+(\d+),(\d+) @@/);
                             if (!hunkHeaderMatch) {
                                 console.log('No hunk header found');
                                 continue;
                             }
-            
                             const startLineOriginal = parseInt(hunkHeaderMatch[1]);
                             const startLineNew = parseInt(hunkHeaderMatch[2]);
                             const lineCountNew = parseInt(hunkHeaderMatch[3]);
                             const endLineNew = startLineNew + lineCountNew - 1;
-            
-                            console.log('Start line original: '+startLineOriginal)
-                            console.log('Start line new: '+startLineNew)
-                            console.log('End line new: '+endLineNew)
-            
+                            console.log('Start line original: ' + startLineOriginal);
+                            console.log('Start line new: ' + startLineNew);
+                            console.log('End line new: ' + endLineNew);
                             const cleanedHunk = hunks[i].replace(/^@@ -\d+,\d+ \+\d+,\d+ @@\n/, '');
-              
-                            const response = await octokit.request('PATCH /repos/'+repo[0]+'/'+repo[1]+'/check-runs/'+options.checkRunID, {
-                                status: 'in_progress',
-                                output: {
-                                    title: 'Veracode Autofix suggestions',
-                                    summary: 'Will create Veracode Autofix suggestions as PR comments',
-                                    text: 'Will create Veracode Autofix suggestions as PR comments',
-                                    annotations: [
-                                        {
-                                        path: flawInfo.sourceFile,
-                                        start_line: startLineOriginal,
-                                        end_line: endLineNew,
-                                        annotation_level: 'warning',
-                                        title: 'Securityy findings',
-                                        message: cleanedHunk,
-                                        }
-                                    ]
-                                },
-                                headers: {
-                                'X-GitHub-Api-Version': '2022-11-28'
-                                }
-                            })
-                            console.log('Check run updated')
-                            console.log(response)
-                        };
+                            /*
+                                            const response = await octokit.request('PATCH /repos/'+repo[0]+'/'+repo[1]+'/check-runs/'+options.checkRunID, {
+                                                status: 'in_progress',
+                                                output: {
+                                                    title: 'Veracode Autofix suggestions',
+                                                    summary: 'Will create Veracode Autofix suggestions as PR comments',
+                                                    text: 'Will create Veracode Autofix suggestions as PR comments',
+                                                    annotations: [
+                                                        {
+                                                        path: flawInfo.sourceFile,
+                                                        start_line: startLineOriginal,
+                                                        end_line: endLineNew,
+                                                        annotation_level: 'warning',
+                                                        title: 'Securityy findings',
+                                                        message: cleanedHunk,
+                                                        }
+                                                    ]
+                                                },
+                                                headers: {
+                                                'X-GitHub-Api-Version': '2022-11-28'
+                                                }
+                                            })
+                                            console.log('Check run updated')
+                                            console.log(response)
+                            */
+                        }
+                        ;
                     }
-            
-            */
+                }
+            }
         }
         catch (error) {
             console.log(error.request);
--- src/checkRun.ts
+++ src/checkRun.ts
@@ -159,69 +159,76 @@ export async function updateCheckRunUpdateBatch(options:any, batchFixResults:any
 
         console.log('Batch Fix Results:')
         console.log(batchFixResults)
-/*        
+       
 
         //Let's check if there are multiple hunks on the first fix result
         let hunks = 0
-        if (batchFixResults[0].indexOf('@@') > 0){
-            //first remove the first part of the result that include the file names and path, we don't need that for the annotation
-            const firstFixResult = batchFixResults[0]
-            const cleanedResults = firstFixResult.replace(/^---.*$\n?|^\+\+\+.*$\n?/gm, '');
-            const hunks = cleanedResults.split(/(?=@@ -\d+,\d+ \+\d+,\d+ @@\n)/);
-            console.log('hunks:')
-            console.log(hunks)
-            const hunksCount = hunks.length
-            console.log('Number of hunks: '+hunksCount)
+        for (let key in batchFixResults.results) {
+            let patches = batchFixResults.results[key].patch;
+            for (let i = 0; i < patches.length; i++) {
+                let patch = patches[i];
 
-           
-            for (let i = 0; i < hunksCount; i++) {
-                
-                const hunkLines = hunks[i].split('\n');
-                const hunkHeader = hunkLines[0];
-                const hunkHeaderMatch = hunkHeader.match(/@@ -(\d+),\d+ \+(\d+),(\d+) @@/);
-                if (!hunkHeaderMatch) {
-                    console.log('No hunk header found');
-                    continue;
-                }
+                console.log('Patch:')
+                console.log(patch)
 
-                const startLineOriginal = parseInt(hunkHeaderMatch[1]);
-                const startLineNew = parseInt(hunkHeaderMatch[2]);
-                const lineCountNew = parseInt(hunkHeaderMatch[3]);
-                const endLineNew = startLineNew + lineCountNew - 1;
+                if (patch.indexOf('@@') > 0) {
+                    const cleanedPatch = patch.replace(/^---.*$\n?|^\+\+\+.*$\n?/gm, '');
+                    const hunks = cleanedPatch.split(/(?=@@ -\d+,\d+ \+\d+,\d+ @@\n)/);
+                    console.log('hunks:');
+                    console.log(hunks);
+                    const hunksCount = hunks.length;
+                    console.log('Number of hunks: ' + hunksCount);
 
-                console.log('Start line original: '+startLineOriginal)
-                console.log('Start line new: '+startLineNew)
-                console.log('End line new: '+endLineNew)
+                    for (let j = 0; j < hunksCount; j++) {
+                        const hunkLines = hunks[j].split('\n');
+                        const hunkHeader = hunkLines[0];
+                        const hunkHeaderMatch = hunkHeader.match(/@@ -(\d+),\d+ \+(\d+),(\d+) @@/);
+                        if (!hunkHeaderMatch) {
+                            console.log('No hunk header found');
+                            continue;
+                        }
 
-                const cleanedHunk = hunks[i].replace(/^@@ -\d+,\d+ \+\d+,\d+ @@\n/, '');
-  
-                const response = await octokit.request('PATCH /repos/'+repo[0]+'/'+repo[1]+'/check-runs/'+options.checkRunID, {
-                    status: 'in_progress',
-                    output: {
-                        title: 'Veracode Autofix suggestions',
-                        summary: 'Will create Veracode Autofix suggestions as PR comments',
-                        text: 'Will create Veracode Autofix suggestions as PR comments',
-                        annotations: [
-                            {
-                            path: flawInfo.sourceFile,
-                            start_line: startLineOriginal,
-                            end_line: endLineNew,
-                            annotation_level: 'warning',
-                            title: 'Securityy findings',
-                            message: cleanedHunk,
+                        const startLineOriginal = parseInt(hunkHeaderMatch[1]);
+                        const startLineNew = parseInt(hunkHeaderMatch[2]);
+                        const lineCountNew = parseInt(hunkHeaderMatch[3]);
+                        const endLineNew = startLineNew + lineCountNew - 1;
+
+                        console.log('Start line original: '+startLineOriginal)
+                        console.log('Start line new: '+startLineNew)
+                        console.log('End line new: '+endLineNew)
+
+                        const cleanedHunk = hunks[i].replace(/^@@ -\d+,\d+ \+\d+,\d+ @@\n/, '');
+        /* 
+                        const response = await octokit.request('PATCH /repos/'+repo[0]+'/'+repo[1]+'/check-runs/'+options.checkRunID, {
+                            status: 'in_progress',
+                            output: {
+                                title: 'Veracode Autofix suggestions',
+                                summary: 'Will create Veracode Autofix suggestions as PR comments',
+                                text: 'Will create Veracode Autofix suggestions as PR comments',
+                                annotations: [
+                                    {
+                                    path: flawInfo.sourceFile,
+                                    start_line: startLineOriginal,
+                                    end_line: endLineNew,
+                                    annotation_level: 'warning',
+                                    title: 'Securityy findings',
+                                    message: cleanedHunk,
+                                    }
+                                ]
+                            },
+                            headers: {
+                            'X-GitHub-Api-Version': '2022-11-28'
                             }
-                        ]
-                    },
-                    headers: {
-                    'X-GitHub-Api-Version': '2022-11-28'
-                    }
-                })
-                console.log('Check run updated')
-                console.log(response)
-            };
+                        })
+                        console.log('Check run updated')
+                        console.log(response)
+        */
+                    };
+                }
+            }
         }
 
-*/
+
     } catch (error:any) {
         console.log(error.request)
         console.log(error.response)
